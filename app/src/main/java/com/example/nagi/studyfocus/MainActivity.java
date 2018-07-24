a4d9fc113ae91434f0f73dec2e8908126323da70
Nagisa Kojima <nk.bg.1208@gmail.com>
2018-07-23 22:50:37 -0700

    dialog added

--- app/src/main/java/com/example/nagi/studyfocus/MainActivity.java
+++ app/src/main/java/com/example/nagi/studyfocus/MainActivity.java
@@ -1,10 +1,20 @@
 package com.example.nagi.studyfocus;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.CountDownTimer;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.afollestad.materialdialogs.MaterialDialog;
 import com.github.mikephil.charting.animation.Easing;
 import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.components.AxisBase;
@@ -23,10 +33,24 @@ import com.github.mikephil.charting.utils.ViewPortHandler;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView timerView;
+    private TextView timerView;
+    private Button setButton;
+    private Button startButton;
+    private Button pauseButton;
+    private Button resetButton;
+
+    private CountDownTimer countDownTimer;
+
+    private boolean mTimerRunning;
+
+    private long startTimeInMillis;
+    private long timeLeftInMills;
+    private long endTime;
+
 
 
     @Override
@@ -34,21 +58,166 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         timerView = findViewById(R.id.timerView);
+        setButton = findViewById(R.id.setButton);
+        startButton = findViewById(R.id.startButton);
+        pauseButton = findViewById(R.id.stopButton);
+        resetButton = findViewById(R.id.resetButton);
 
-        new CountDownTimer(30000, 1000) {
 
+
+
+        setButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                new MaterialDialog.Builder(MainActivity.this)
+                        .title(R.string.time_pick)
+                        .items(R.array.time)
+                        .itemsCallbackSingleChoice(-1, new MaterialDialog.ListCallbackSingleChoice() {
+                            @Override
+                            public boolean onSelection(MaterialDialog dialog, View view, int which, CharSequence text) {
+                                /**
+                                 * If you use alwaysCallSingleChoiceCallback(), which is discussed below,
+                                 * returning false here won't allow the newly selected radio button to actually be selected.
+                                 **/
+                                switch (which){
+                                    case 0:
+                                        which = 10;
+                                        break;
+                                    case 1:
+                                        which = 20;
+                                        break;
+                                    case 2:
+                                        which = 30;
+                                        break;
+                                    case 3:
+                                        which = 40;
+                                        break;
+                                    case 4:
+                                        which = 50;
+                                        break;
+                                    case 5:
+                                        which = 60;
+                                        break;
+                                    default:
+                                        break;
+                                }
+                                String input = Integer.toString(which);
+                                long millsInput = Long.parseLong(input) * 60000;
+                                setTime(millsInput);
+                                return true;
+                            }
+                        })
+                        .positiveText(R.string.okbutton)
+                        .show();
+                
+
+            }
+        });
+
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startTimer();
+            }
+        });
+
+        pauseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mTimerRunning){
+                    pauseTimer();
+                }
+
+            }
+        });
+
+        resetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                resetTimer();
+            }
+        });
+
+    }
+
+
+    private void setTime(long milliseconds) {
+        startTimeInMillis = milliseconds;
+        resetTimer();
+        closeKeyboard();
+    }
+
+
+
+
+    private void startTimer() {
+        endTime = System.currentTimeMillis() + timeLeftInMills;
+
+        countDownTimer = new CountDownTimer(timeLeftInMills,1000) {
+            @Override
             public void onTick(long millisUntilFinished) {
-                timerView.setText(" " + millisUntilFinished / 1000);
+                timeLeftInMills = millisUntilFinished;
+                updateCountDownText();
             }
 
+            @Override
             public void onFinish() {
-                timerView.setText("Focus Done!");
-
+                mTimerRunning = false;
+                updateCountDownText();
             }
         }.start();
 
+        mTimerRunning = true;
+//        updateWatchInterface();
+    }
+
+
+    private void pauseTimer() {
+        countDownTimer.cancel();
+        mTimerRunning = false;
+//        updateWatchInterface();
+    }
+
+
+
+    private void resetTimer() {
+        timeLeftInMills = startTimeInMillis;
+        updateCountDownText();
+//        updateWatchInterface();
+    }
+
+
+
+    private void updateCountDownText() {
+        int hours = (int)(timeLeftInMills / 1000 / 3600);
+        int minutes = (int)((timeLeftInMills / 1000) % 3600) / 60;
+        int seconds = (int) (timeLeftInMills / 1000) % 60;
+
+        String timeLeftFormatted;
+        if (hours > 0){
+            timeLeftFormatted = String.format(Locale.getDefault(),"%d:%02d:%02d",hours,minutes,seconds);
+        }else {
+            timeLeftFormatted = String.format(Locale.getDefault(),"%02d:%02d",minutes,seconds);
+        }
+
+        timerView.setText(timeLeftFormatted);
 
     }
+
+
+//    private void updateWatchInterface() {
+//    }
+
+    private void closeKeyboard() {
+        View view = this.getCurrentFocus();
+        if (view != null) {
+            InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
+    }
+
 
 }
